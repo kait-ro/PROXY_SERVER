5b9a26eef64513ab7e5bf9d8e3a10fbfe866b7a2
Harman Singh Sandhu <harman2504sandhu@gmail.com>
2026-05-03 19:13:56 +0530

    phase 2 of gemma 4 integration done

--- WebsiteFilter.cpp
+++ WebsiteFilter.cpp
@@ -1,6 +1,49 @@
 #include "WebsiteFilter.h"
 #include <fstream>
+#include <iostream>
+
 using namespace std;
+
+string normalizeDomain(string domain)
+{
+    transform(domain.begin(), domain.end(), domain.begin(), ::tolower);
+
+    if (domain.find("http://") == 0)
+        domain = domain.substr(7);
+    else if (domain.find("https://") == 0)
+        domain = domain.substr(8);
+
+    if (domain.find("www.") == 0)
+        domain = domain.substr(4);
+
+    size_t slash = domain.find('/');
+    if (slash != string::npos)
+        domain = domain.substr(0, slash);
+
+    return domain;
+}
+
+
+bool isValidDomain(const string& domain)
+{
+    if (domain.empty())
+        return false;
+
+    if (domain.find('.') == string::npos)
+        return false;
+
+    if (domain.find(' ') != string::npos)
+        return false;
+
+    return true;
+}
+
+WebsiteFilter::WebsiteFilter()
+{
+    loadSites("blocked_sites.txt");
+    loadSites("ai_blocked_sites.txt");  
+}
+
 void WebsiteFilter::loadSites(const string& filename)
 {
     ifstream file(filename);
@@ -8,29 +51,33 @@ void WebsiteFilter::loadSites(const string& filename)
 
     while (file >> site)
     {
+        site = normalizeDomain(site);
+
+        if (!isValidDomain(site))
+            continue;
+
         blockedSites.insert(site);
     }
-
     file.close();
 }
 
 bool WebsiteFilter::isAllowed(const string& domain)
 {
-    for (const auto& site : blockedSites)
+    string d = normalizeDomain(domain);
+
+    if (blockedSites.find(d) != blockedSites.end())
+        return false;
+
+    size_t pos = d.find('.');
+
+    while (pos != string::npos)
     {
-        if (domain == site)
+        string sub = d.substr(pos + 1);
+
+        if (blockedSites.find(sub) != blockedSites.end())
             return false;
 
-        if (domain.size() > site.size())
-        {
-            size_t pos = domain.size() - site.size();
-
-            if (domain.compare(pos, site.size(), site) == 0 &&
-                domain[pos - 1] == '.')
-            {
-                return false;
-            }
-        }
+        pos = d.find('.', pos + 1);
     }
 
     return true;
--- WebsiteFilter.h
+++ WebsiteFilter.h
@@ -1,6 +1,7 @@
 #ifndef WEBSITE_FILTER_H
 #define WEBSITE_FILTER_H
 #include <string>
+#include <algorithm> 
 #include <unordered_set>
 
 class WebsiteFilter {
@@ -8,6 +9,7 @@ private:
     std::unordered_set<std::string> blockedSites;
 
 public:
+    WebsiteFilter();
     void loadSites(const std::string& filename);
     bool isAllowed(const std::string& domain);
 };
