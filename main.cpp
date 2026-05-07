11255d9086d8808e730766ca76a85a0ebfcfc17f
Harman Singh Sandhu <harman2504sandhu@gmail.com>
2026-05-07 10:45:29 +0530

    all code formatting

--- main.cpp
+++ main.cpp
@@ -1,45 +1,45 @@
 #include <iostream>
 #include "proxyserver.h"
 #include "Authenticator.h"
+
 using namespace std;
+
 int main()
 {
-Authenticator auth;
-auth.loadUsers("users.txt");
-string choice;
-cout << "1. Login\n2. Signup\nChoice: ";
-cin >> choice;
-string username, password, role;
-if (choice == "2")
-{
-cout << "Enter username: ";
-cin >> username;
-cout << "Enter password: ";
-cin >> password;
-cout << "Enter role (admin/user): ";
-cin >> role;
-if (auth.signup(username, password, role))
-    cout << "Signup successful!\n";
-else
-    cout << "Username already exists. Please log in instead.\n";
-}
-while (true)
-{
-cout << "\nLogin\nUsername: ";
-cin >> username;
-cout << "Password: ";
-cin >> password;
-role = auth.login(username, password);
-if (role != "")
-{
-cout << "Login successful as " << role << endl;
-break;
-}
-else
-{
-cout << "Invalid credentials, try again.\n";
-}
-}
+    Authenticator auth;
+    auth.loadUsers("users.txt");
+    string choice;
+    cout << "1. Login\n2. Signup\nChoice: ";
+    cin >> choice;
+    string username, password, role;
+    if (choice == "2") {
+        cout << "Enter username: ";
+        cin >> username;
+        cout << "Enter password: ";
+        cin >> password;
+        cout << "Enter role (admin/user): ";
+        cin >> role;
+        if (auth.signup(username, password, role))
+            cout << "Signup successful!\n";
+        else
+            cout << "Username already exists. Please log in instead.\n";
+        }
+    while (true) {
+    cout << "\nLogin\nUsername: ";
+    cin >> username;
+    cout << "Password: ";
+    cin >> password;
+    role = auth.login(username, password);
+    if (role != "")
+    {
+        cout << "Login successful as " << role << endl;
+        break;
+        }
+        else
+        {
+            cout << "Invalid credentials, try again.\n";
+    }
+    }
 ProxyServer proxy(8080);
 proxy.setUser(username, role);
 proxy.startServer();
